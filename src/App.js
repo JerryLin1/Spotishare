f2cc0f60a7c1761a7ad7af2c38bbc436bdb4ddc2
Jerry Lin <jerrylin9969@gmail.com>
2021-12-23 23:22:53 -0500

    thing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ var SpotifyWebApi = require('spotify-web-api-node');
 
 // credentials are optional
 var spotifyApi = new SpotifyWebApi({
-  clientId: '3f25e7854a2645b78bd43d3f3003f105',
-  clientSecret: 'aee85bacd18b4301ad31b12197149759',
+  clientId: '',
+  clientSecret: '',
   redirectUri: 'http://www.example.com/callback'
 });
 
