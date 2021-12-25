65d737a53edfdf08d71be7d57fe1b377e84e61e7
unknown <roseak.lin@gmail.com>
2021-12-25 16:46:30 -0500

    merge

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,17 @@
-<<<<<<< HEAD
-import React from "react";
-=======
-
 import { useState } from "react";
->>>>>>> adbc5a9028e59628753c73fb70f2e5a0d8083a09
 
 import { Row, Col } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-<<<<<<< HEAD
-export default class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            members: ["Royeek", "Yom", "Yerry"],
-            songs: null,
-        };
+function HomePage(props) {
+    const [members, setMembers] = useState(["Royeek", "Yom", "Yerry"]);
+    const [songs, setSongs] = useState(null);
 
-        this.client = this.props.client;
-    }
-
-    renderMembers() {
+    const renderMembers = () => {
         return (
             <div>
-                {this.state.members.map((member, key) => {
+                {members.map((member, key) => {
                     if (member === "Royeek") {
                         return (
                             <div className="member-card" key={key}>
@@ -47,153 +34,69 @@ export default class HomePage extends React.Component {
                         </div>
                     );
                 })}
-=======
-function HomePage(props) {
-  const [members, setMembers] = useState(["Royeek", "Yom", "Yerry"]);
-  const [songs, setSongs] = useState(null);
-
-
-  const renderMembers = () => {
-    return (
-      <div>
-        {members.map((member, key) => {
-          if (member === "Royeek") {
-            return (
-              <div className="member-card" key={key}>
-                {member}{" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={36}
-                  height={20}
-                  viewBox="0 0 512 512"
-                  fill="gold"
-                >
-                  <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
-                </svg>
-              </div>
-            );
-          }
-          return (
-            <div className="member-card" key={key}>
-              {member}
->>>>>>> adbc5a9028e59628753c73fb70f2e5a0d8083a09
             </div>
         );
-    }
+    };
 
-<<<<<<< HEAD
-    renderSongList() {
-        if (this.state.songs === null) {
+    const renderSongList = () => {
+        if (songs === null) {
             return null;
         } else {
-            return this.state.songs;
+            return songs;
         }
-=======
-  const renderSongList = () => {
-    if (songs === null) {
-      return <span />;
-    } else {
-      return songs;
->>>>>>> adbc5a9028e59628753c73fb70f2e5a0d8083a09
-    }
+    };
 
-<<<<<<< HEAD
-    render() {
-        return (
-            <div>
-                <h1 id="title">SpotiShare</h1>
-                <button
-                    onClick={() => {
-                        if (IsLoggedIn()) CreateLobby();
-                        else Login();
-                    }}
-                >
-                    Create Lobby
-                </button>
-                <button
-                    onClick={() => {
-                        Login();
-                    }}
-                    id="sign-in"
-                >
-                    Sign in
-                </button>
+    return (
+        <div>
+            <h1 id="title">SpotiShare</h1>
+            {/*  */}
+            <button
+                onClick={() => {
+                    if (IsLoggedIn()) {
+                        console.log(IsLoggedIn());
+                        CreateLobby();
+                    } else Login();
+                }}
+            >
+                Create Lobby
+            </button>
+            <button
+                onClick={() => {
+                    Login();
+                }}
+                id="sign-in"
+            >
+                Sign in
+            </button>
 
-                <Row style={{ margin: "3em 1.5em 0 1.5em" }}>
-                    <Col xs="5" id="left">
-                        <h3>Current Listening Party Members:</h3>
-                        <div>{this.renderMembers()}</div>
-                    </Col>
-                    <Col xs="1"></Col>
-                    <Col id="right">
-                        <div id="searchArea">
-                            <input
-                                id="searchbox"
-                                type="text"
-                                placeholder="Artist, Song Name, Album..."
-                            />
-                            <button id="search-btn">Search!</button>
-                        </div>
-                        <button
-                            onClick={() => {
-                                if (IsLoggedIn()) {
-                                    // just testing api stuff
-                                    fetch(
-                                        `/top?accessToken=${localStorage.getItem(
-                                            "spotify-access-token"
-                                        )}`
-                                    )
-                                        .then((e) => e.json())
-                                        .then((data) => {
-                                            console.log(data);
-                                            setSongs(
-                                                <div>
-                                                    {data.items.map((e) => (
-                                                        <div className="song-card">
-                                                            <div className="song-card">
-                                                                <iframe
-                                                                    src={`https://open.spotify.com/embed/track/${e.id}?utm_source=generator`}
-                                                                    width="100%"
-                                                                    height="80"
-                                                                    frameBorder="0"
-                                                                    allowfullscreen=""
-                                                                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                                                                ></iframe>
-                                                            </div>
-                                                        </div>
-                                                    ))}
-                                                </div>
-                                            );
-                                        })
-                                        .catch((err) => {
-                                            console.log(err);
-                                            Login();
-                                        });
-                                } else {
-                                    Login();
-                                }
-                            }}
-                        >
-                            CLICK ME
-                        </button>
-                        <div id="song-list">{this.renderSongList()}</div>
-                    </Col>
-                </Row>
-
-                <button
-                    onClick={() => {
-                        if (IsLoggedIn()) {
-                            // just testing api stuff
-                            fetch(
-                                `/top?accessToken=${localStorage.getItem(
-                                    "spotify-access-token"
-                                )}`
-                            )
-                                .then((e) => e.json())
-                                .then((data) => {
-                                    console.log(data);
-                                    this.setState({
-                                        songs: (
+            <Row style={{ margin: "3em 1.5em 0 1.5em" }}>
+                <Col xs="5">
+                    <h3>Current Listening Party Members:</h3>
+                    <div>{renderMembers()}</div>
+                </Col>
+                <Col xs="1"></Col>
+                <Col>
+                    <div id="searchArea">
+                        <input
+                            id="searchbox"
+                            type="text"
+                            placeholder="Artist, Song Name, Album..."
+                        />
+                        <button id="search-btn">Search!</button>
+                    </div>
+                    <button
+                        onClick={() => {
+                            if (IsLoggedIn()) {
+                                // just testing api stuff
+                                fetch(
+                                    `/top?accessToken=${localStorage.getItem(
+                                        "spotify-access-token"
+                                    )}`
+                                )
+                                    .then((e) => e.json())
+                                    .then((data) => {
+                                        console.log(data);
+                                        setSongs(
                                             <div>
                                                 {data.items.map((e) => (
                                                     <div className="song-card">
@@ -210,123 +113,30 @@ function HomePage(props) {
                                                     </div>
                                                 ))}
                                             </div>
-                                        ),
+                                        );
+                                    })
+                                    .catch((err) => {
+                                        console.log(err);
+                                        Login();
                                     });
-                                })
-                                .catch((err) => {
-                                    console.log(err);
-                                    Login();
-                                });
-                        } else {
-                            Login();
-                        }
-                    }}
-                >
-                    CLICK ME
-                </button>
-                <div>{this.renderSongList()}</div>
-            </div>
-        );
-    }
-=======
-
-  return (
-    <div>
-      <h1 id="title">SpotiShare</h1>
-      {/*  */}
-      <button onClick={() => {
-        if (IsLoggedIn()) CreateLobby();
-        else Login()
-      }}>
-        Create Lobby
-      </button>
-      <button
-        onClick={() => {
-          Login()
-        }}
-        id="sign-in"
-      >
-        Sign in
-      </button>
-
-      <Row style={{ margin: "3em 1.5em 0 1.5em" }}>
-        <Col xs="5">
-          <h3>Current Listening Party Members:</h3>
-          <div>{renderMembers()}</div>
-        </Col>
-        <Col xs="1"></Col>
-        <Col>
-          <div id="searchArea">
-            <input
-              id="searchbox"
-              type="text"
-              placeholder="Artist, Song Name, Album..."
-            />
-            <button id="search-btn">Search!</button>
-          </div>
-          <button
-            onClick={() => {
-              if (
-                IsLoggedIn()
-              ) {
-                // just testing api stuff
-                fetch(
-                  `/top?accessToken=${localStorage.getItem(
-                    "spotify-access-token"
-                  )}`
-                )
-                  .then((e) => e.json())
-                  .then((data) => {
-                    console.log(data);
-                    setSongs(
-                      <div>
-                        {data.items.map((e) => (
-                          <div className="song-card">
-                            <div className="song-card">
-                              <iframe
-                                src={`https://open.spotify.com/embed/track/${e.id}?utm_source=generator`}
-                                width="100%"
-                                height="80"
-                                frameBorder="0"
-                                allowfullscreen=""
-                                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                              ></iframe>
-                            </div>
-                          </div>
-                        ))}
-                      </div>
-                    )
-                  })
-                  .catch((err) => {
-                    console.log(err);
-                    Login();
-                  });
-              } else {
-                Login();
-              }
-            }}
-          >
-            CLICK ME
-          </button>
-          <div>{renderSongList()}</div>
-        </Col>
-      </Row>
-    </div>
-  );
->>>>>>> adbc5a9028e59628753c73fb70f2e5a0d8083a09
+                            } else {
+                                Login();
+                            }
+                        }}
+                    >
+                        CLICK ME
+                    </button>
+                    <div id="song-list">{renderSongList()}</div>
+                </Col>
+            </Row>
+        </div>
+    );
 }
-
-async function IsLoggedIn() {
-<<<<<<< HEAD
+function IsLoggedIn() {
     return (
         localStorage.getItem("spotify-access-token") &&
         localStorage.getItem("spotify-access-token-expiry") > Date.now()
     );
-=======
-  return localStorage.getItem("spotify-access-token") &&
-    localStorage.getItem("spotify-access-token-expiry") >
-    Date.now()
->>>>>>> adbc5a9028e59628753c73fb70f2e5a0d8083a09
 }
 async function Login() {
     fetch("/auth/login")
@@ -340,7 +150,6 @@ async function Login() {
 }
 
 async function CreateLobby() {
-<<<<<<< HEAD
     fetch(
         `/createLobby?accessToken=${localStorage.getItem(
             "spotify-access-token"
@@ -354,16 +163,5 @@ async function CreateLobby() {
             alert(error);
         });
 }
-=======
-  fetch(`/createLobby?accessToken=${localStorage.getItem("spotify-access-token")}`)
-    .then(e => e.json())
-    .then(data => {
-      window.location = data.roomId
-    })
-    .catch(error => {
-      alert(error)
-    })
-}
 
 export default HomePage;
->>>>>>> adbc5a9028e59628753c73fb70f2e5a0d8083a09
