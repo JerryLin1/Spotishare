21c474d35e8484a897197c4ebafaf2c79223ee45
unknown <roseak.lin@gmail.com>
2021-12-27 12:47:42 -0500

    merge

--- src/Lobby.jsx
+++ src/Lobby.jsx
@@ -23,12 +23,13 @@ function Lobby(props) {
         "185Wm4Mx09dQG0fUktklDm?si=8fd67a8eb5f04c99",
         "3dPtXHP0oXQ4HCWHsOA9js?si=8593d745abde4cb7",
         "185Wm4Mx09dQG0fUktklDm?si=8fd67a8eb5f04c99",
-        "185Wm4Mx09dQG0fUktklDm?si=8fd67a8eb5f04c99",
+        "185Wm4Mx09dQG0fUktklDm?si=8fd67a8eb5f04c99", 
     ]);
 
     const renderMembers = () => {
         let newMemberList = [];
         for (const user of members) {
+            console.log(user);
             newMemberList.push(
                 <Row className="member-card">
                     <Col id="member-name">
@@ -44,82 +45,6 @@ function Lobby(props) {
                     </Col>
                 </Row>
             );
-            newMemberList.push(
-                <Row className="member-card">
-                    <Col id="member-name">
-                        {user.name}
-                        {user.isHost && (
-                            <svg xmlns="http://www.w3.org/2000/svg" width={36} height={20} viewBox="0 0 512 512" fill="gold">
-                                <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
-                            </svg>
-                        )}
-                    </Col>
-                    <Col style={{ display: "flex", justifyContent: "right", padding: "0" }}>
-                        <img id="member-pfp" src={user.image}></img>
-                    </Col>
-                </Row>
-            );
-            newMemberList.push(
-                <Row className="member-card">
-                    <Col id="member-name">
-                        {user.name}
-                        {user.isHost && (
-                            <svg xmlns="http://www.w3.org/2000/svg" width={36} height={20} viewBox="0 0 512 512" fill="gold">
-                                <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
-                            </svg>
-                        )}
-                    </Col>
-                    <Col style={{ display: "flex", justifyContent: "right", padding: "0" }}>
-                        <img id="member-pfp" src={user.image}></img>
-                    </Col>
-                </Row>
-            );
-            newMemberList.push(
-                <Row className="member-card">
-                    <Col id="member-name">
-                        {user.name}
-                        {user.isHost && (
-                            <svg xmlns="http://www.w3.org/2000/svg" width={36} height={20} viewBox="0 0 512 512" fill="gold">
-                                <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
-                            </svg>
-                        )}
-                    </Col>
-                    <Col style={{ display: "flex", justifyContent: "right", padding: "0" }}>
-                        <img id="member-pfp" src={user.image}></img>
-                    </Col>
-                </Row>
-            );
-            newMemberList.push(
-                <Row className="member-card">
-                    <Col id="member-name">
-                        {user.name}
-                        {user.isHost && (
-                            <svg xmlns="http://www.w3.org/2000/svg" width={36} height={20} viewBox="0 0 512 512" fill="gold">
-                                <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
-                            </svg>
-                        )}
-                    </Col>
-                    <Col style={{ display: "flex", justifyContent: "right", padding: "0" }}>
-                        <img id="member-pfp" src={user.image}></img>
-                    </Col>
-                </Row>
-            );
-            // console.log(user);
-            // newMemberList.push(
-            //     <Row className="member-card">
-            //         <Col id="member-name">
-            //             {user.name}
-            //             {user.isHost && (
-            //                 <svg xmlns="http://www.w3.org/2000/svg" width={36} height={20} viewBox="0 0 512 512" fill="gold">
-            //                     <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
-            //                 </svg>
-            //             )}
-            //         </Col>
-            //         <Col style={{ display: "flex", justifyContent: "right", padding: "0" }}>
-            //             <img id="member-pfp" src={user.image}></img>
-            //         </Col>
-            //     </Row>
-            // );
         }
 
         return newMemberList;
@@ -179,8 +104,8 @@ function Lobby(props) {
                     <Queue queue={queue} />
                 </Col>
                 <Col>
-                    <Card>
-                        <Card.Header>Chat</Card.Header>
+                    <Card className="chat-container">
+                        <Card.Header id="chat-header">Chat</Card.Header>
                         <div className="dropdown" onClick={toggleLobbyList}>
                             <p style={{ margin: "0" }}>Current Listening Party Members:</p>
                             <CaretDownFill style={{ position: "absolute", right: "1em", top: "50%", transform: "translateY(-50%)" }} />
--- src/WebPlayback.jsx
+++ src/WebPlayback.jsx
@@ -102,7 +102,6 @@ function WebPlayback(props) {
         return (
             <>
                 <div className="container">
-                    <h3 style={{ textAlign: "center" }}>Currently Playing:</h3>
                     <div className="main-wrapper">
                         
                         <img height="500px" width="500px" src={current_track.album.images[0].url} className="now-playing__cover" alt="cover image" />
