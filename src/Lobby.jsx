cab59a4a71136d7272488eaa5bf1a15cf2339bce
unknown <roseak.lin@gmail.com>
2021-12-28 14:01:35 -0500

    merge

--- src/Lobby.jsx
+++ src/Lobby.jsx
@@ -38,13 +38,7 @@ function Lobby(props) {
                     <Col id="member-name">
                         {user.name}
                         {user.isHost && (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width={36}
-                                height={20}
-                                viewBox="0 0 512 512"
-                                fill="gold"
-                            >
+                            <svg xmlns="http://www.w3.org/2000/svg" width={36} height={20} viewBox="0 0 512 512" fill="gold">
                                 <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
                             </svg>
                         )}
@@ -62,11 +56,7 @@ function Lobby(props) {
     };
 
     const initializeUser = () => {
-        fetch(
-            `/joinLobby?roomId=${roomId}&accessToken=${localStorage.getItem("spotify-access-token")}&socketid=${
-                client.socket.id
-            }`
-        ).then((data) => {
+        fetch(`/joinLobby?roomId=${roomId}&accessToken=${localStorage.getItem("spotify-access-token")}&socketid=${client.socket.id}`).then((data) => {
             if (data.status === 200) {
                 client.socket.emit("joinRoom", { roomId: roomId }, (response) => {
                     client.isHost = response.isHost;
@@ -125,11 +115,7 @@ function Lobby(props) {
             <div className="page-title unselectable">SpotiShare</div>
             <Row>
                 <Col xl="8">
-                    <WebPlayback
-                        roomId={roomId}
-                        disabled={client.isHost}
-                        token={localStorage.getItem("spotify-access-token")}
-                    />
+                    <WebPlayback roomId={roomId} disabled={client.isHost} token={localStorage.getItem("spotify-access-token")} />
                 </Col>
                 <Col>
                     <Card className="chat-container">
@@ -161,10 +147,10 @@ function Lobby(props) {
                 </Col>
             </Row>
             <Row>
-                <Col sm="6" xl="8">
+                <Col xs="6" xl="8" id="queue">
                     <Queue queue={queue} />
                 </Col>
-                <Col>
+                <Col id="search-conatiner">
                     <div id="searchArea">
                         <h3 className="unselectable">Find a Song!</h3>
                         <input
@@ -174,9 +160,9 @@ function Lobby(props) {
                                 updateTypingTimeout(
                                     setTimeout(() => {
                                         fetch(
-                                            `/search?value=${
-                                                searchInputRef.current.value
-                                            }&accessToken=${localStorage.getItem("spotify-access-token")}`
+                                            `/search?value=${searchInputRef.current.value}&accessToken=${localStorage.getItem(
+                                                "spotify-access-token"
+                                            )}`
                                         )
                                             .then((e) => e.json())
                                             .then((data) => {
--- src/Queue.jsx
+++ src/Queue.jsx
@@ -4,38 +4,36 @@ function Queue(props) {
     const queue = props.queue;
 
     return (
-        <div id="queue">
-            <div id="queue-container">
-                <h2 id="queue-title" className="unselectable">
-                    Next In Queue
-                </h2>
-                {queue.map((item, key) => {
-                    return (
-                        <Row key={key} className="song-card">
-                            <Col xs={10} style={{ padding: "0 0.5em" }}>
-                                <iframe
-                                    src={`https://open.spotify.com/embed/track/${item}?utm_source=generator`}
-                                    width="100%"
-                                    height="80"
-                                    frameBorder="0"
-                                    allowFullScreen=""
-                                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                                ></iframe>
-                            </Col>
-                            <Col
-                                style={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    justifyContent: "center",
-                                    padding: "0 0.5em",
-                                }}
-                            >
-                                <button>Remove from Queue</button>
-                            </Col>
-                        </Row>
-                    );
-                })}
-            </div>
+        <div id="queue-container">
+            <h2 id="queue-title" className="unselectable">
+                Next In Queue
+            </h2>
+            {queue.map((item, key) => {
+                return (
+                    <Row key={key} className="song-card">
+                        <Col xs={12} xl={10} style={{ padding: "0 0.5em", alignItems: "center" }}>
+                            <iframe
+                                src={`https://open.spotify.com/embed/track/${item}?utm_source=generator`}
+                                width="100%"
+                                height="80"
+                                frameBorder="0"
+                                allowFullScreen=""
+                                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                            ></iframe>
+                        </Col>
+                        <Col
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                padding: "0 0.5em",
+                            }}
+                        >
+                            <button>Remove</button>
+                        </Col>
+                    </Row>
+                );
+            })}
         </div>
     );
 }
