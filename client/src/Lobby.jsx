6688bdf1f1142c805030aa061f40ac2e5d9990ca
unknown <roseak.lin@gmail.com>
2022-01-06 23:04:18 -0500

    reverted searchbar css and other ui changes

--- client/src/Lobby.jsx
+++ client/src/Lobby.jsx
@@ -18,23 +18,20 @@ function Lobby(props) {
     const [queue, updateQueue] = useState([]);
 
     const renderMembers = () => {
-        /* TODO: make member cards not ugly */
         let newMemberList = [];
         for (const user of members) {
             newMemberList.push(
                 <Row className="member-card">
-                    <Col xs="12" style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "0" }}>
-                        <img id="member-pfp" src={user.image} />
+                    <Col id="member-name" style={{ padding: "0" }}>
+                        {user.name}
+                        {user.isHost && (
+                            <svg xmlns="http://www.w3.org/2000/svg" width={36} height={20} viewBox="0 0 512 512" fill="gold">
+                                <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
+                            </svg>
+                        )}
                     </Col>
-                    <Col>
-                        <div>
-                            {user.isHost && (
-                                <svg xmlns="http://www.w3.org/2000/svg" width={36} height={20} viewBox="0 0 512 512" fill="gold">
-                                    <path d="M528 448H112c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h416c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm64-320c-26.5 0-48 21.5-48 48 0 7.1 1.6 13.7 4.4 19.8L476 239.2c-15.4 9.2-35.3 4-44.2-11.6L350.3 85C361 76.2 368 63 368 48c0-26.5-21.5-48-48-48s-48 21.5-48 48c0 15 7 28.2 17.7 37l-81.5 142.6c-8.9 15.6-28.9 20.8-44.2 11.6l-72.3-43.4c2.7-6 4.4-12.7 4.4-19.8 0-26.5-21.5-48-48-48S0 149.5 0 176s21.5 48 48 48c2.6 0 5.2-.4 7.7-.8L128 416h384l72.3-192.8c2.5.4 5.1.8 7.7.8 26.5 0 48-21.5 48-48s-21.5-48-48-48z" />
-                                </svg>
-                            )}
-                        </div>
-                        <div>{user.name}</div>
+                    <Col xs="auto" style={{ padding: "0" }}>
+                        <img id="member-pfp" src={user.image} />
                     </Col>
                 </Row>
             );
@@ -54,6 +51,7 @@ function Lobby(props) {
     };
 
     const addToQueue = (queueItem) => {
+        document.getElementsByClassName("dropdown")[0].style.display = "inherit";
         updateQueue((oldQueue) => [...oldQueue, queueItem]);
     };
 
@@ -63,6 +61,9 @@ function Lobby(props) {
             newQueue.splice(key, 1);
             return newQueue;
         });
+        if (queue.length() === 0) {
+            document.getElementsByClassName("dropdown")[0].style.display = "none";
+        }
     };
 
     const toggleDropdown = (dropdown) => {
@@ -124,19 +125,16 @@ function Lobby(props) {
             <Row>
                 <Col xs={12} xl={3}>
                     <Queue queue={queue} />
-                    <div className="dropdown unselectable" onClick={() => toggleDropdown("export-settings")}>
+                    <div className="dropdown unselectable" onClick={() => toggleDropdown("export-settings")} style={{ display: "none" }}>
                         Export Playlist
                         <CaretDownFill className="caret" />
                     </div>
                     <div className="export-settings">
                         <button>
-                            Export playlist to Spotify <Spotify fill="#23da63" />
+                            Export Spotify Playlist <Spotify fill="#23da63" />
                         </button>
                         <button>
-                            Export only good songs to Spotify <Spotify fill="#23da63" />
-                        </button>
-                        <button>
-                            soptify <Spotify fill="#23da63" />
+                            Export others' songs to Spotify Playlist <Spotify fill="#23da63" />
                         </button>
                     </div>
                 </Col>
@@ -185,8 +183,7 @@ function processChatMessage(text) {
 }
 
 function formatMessage(msg) {
-    console.log(msg.replaceAll("<", "&lt;").replaceAll(">", "&gt;"))
-    return msg.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    return msg.replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("&", "&amp;");
 }
 
 export default Lobby;
--- client/src/SearchBar.jsx
+++ client/src/SearchBar.jsx
@@ -18,35 +18,35 @@ function SearchBar(props) {
         let searchArea = document.querySelector("#searchArea"),
             searchBox = document.querySelector("#searchbox");
         if (window.innerWidth <= 1200) {
-            searchArea.style.cssText =
-                "position: fixed !important; width: 100vw; height: 100%; top: 0; left: 0; background-color: rgba(25,25,25,0.75); align-items: center";
-            searchBox.style.cssText = "width: 60%";
+            searchArea.style.cssText = `position: fixed !important; width: 100vw; height: 100%; top: 0; background-color: rgba(25,25,25,0.75)`;
+            searchBox.style.cssText = "position: absolute; left: 50%; transform: translateX(-50%); width: 60%";
 
             document.querySelector("#result-list").style.cssText = "opacity: 1; display: block";
-            document.querySelector("#searchArea-close").style.display = "block";
         } else {
-            searchArea.style.cssText =
-                "position: fixed !important; width: 100vw; top: 0; left: 0; background-color: rgba(25,25,25,0.75); align-items: center";
-
-            document.querySelector("#result-list").style.display = "block";
-            document.querySelector("#searchArea-close").style.display = "block";
+            searchArea.style.cssText = "position: fixed !important; width: 100vw; height: 12em";
+            searchBox.style.cssText = "left: 50vw; transform: translateX(-50%)";
 
             anime({
                 targets: searchArea,
-                height: ["40.5em", `${window.innerHeight / 16}em`],
+                height: `${window.innerHeight / 16}em`,
                 width: "100vw",
                 backgroundColor: "rgba(33,33,33,0.75)",
                 easing: "linear",
                 duration: 250,
             });
 
-            anime({ targets: "#result-list", opacity: 1, duration: 100, easing: "linear" });
-
             anime({
                 targets: searchBox,
                 width: ["calc(33.33333% - 4em)", "60%"],
                 easing: "linear",
                 duration: 250,
+                complete: () => {
+                    document.querySelector("#result-list").style.display = "block";
+                    anime({ targets: "#result-list", opacity: 1, duration: 100, easing: "linear" });
+                    if (window.innerHeight > 700) {
+                        document.querySelector("#searchArea-close").style.display = "block";
+                    }
+                },
             });
         }
     };
@@ -55,25 +55,23 @@ function SearchBar(props) {
     const shrinkSearchArea = () => {
         let searchArea = document.querySelector("#searchArea"),
             searchBox = document.querySelector("#searchbox");
-
+        document.querySelector("#searchArea-close").style.display = "none";
         if (window.innerWidth <= 1200) {
             document.querySelector("#result-list").style.cssText = "opacity: 0; display: none";
-            document.querySelector("#searchArea-close").style.display = "none";
-            searchArea.style.cssText = "width: 100%; align-items: start";
-            searchBox.style.cssText = "left: 0; transform: none; width: 80%";
+            searchArea.style.cssText = "position: relative; width: 100%; text-align: center";
+            searchBox.style.cssText = "transform: none; width: 80%";
         } else {
-            document.querySelector("#searchArea-close").style.display = "none";
             anime({ targets: "#result-list", opacity: 0, duration: 100, easing: "linear" });
+            document.querySelector("#result-list").style.display = "none";
 
             anime({
                 targets: searchArea,
-                height: [`${window.innerHeight / 16}em`, "40.5em"],
+                height: [`${window.innerHeight / 16}em`, "12em"],
                 backgroundColor: "rgba(0,0,0,0)",
                 easing: "linear",
                 duration: 250,
                 complete: () => {
-                    searchArea.style.cssText = "width: 100%; align-items: start;";
-                    document.querySelector("#result-list").style.display = "none";
+                    searchArea.style.cssText = "position: relative; width: 100%; height: 0";
                 },
             });
 
@@ -92,7 +90,6 @@ function SearchBar(props) {
     return (
         <div
             onClick={(e) => {
-                // onClick event to close search area if it is visible and they did not click on the result list or input field
                 if (document.querySelector("#result-list").style.opacity !== "1") {
                     expandSearchArea();
                 } else if (e.target.id === "searchArea") {
@@ -127,48 +124,42 @@ function SearchBar(props) {
                 placeholder="Artist, Song Name, Album..."
             />
             <div id="result-list">
-                {searchResults.length === 0 ? (
-                    <h1 style={{ textAlign: "center", width: "100%" }}>Search up some songs!</h1>
-                ) : (
-                    searchResults.map((item, key) => {
-                        return (
-                            <Row key={key} className="song-card">
-                                <Col xs={12} xl={9} style={{ padding: "0.5em" }}>
-                                    <Row style={{ width: "100%" }}>
-                                        <Col xs={6}>
-                                            <div style={{ margin: "0.5em", textAlign: "center" }}>
-                                                <img className="unselectable" src={item.album.images[1].url} />
-                                                <p style={{ fontSize: "0.75em", margin: 0 }}>
-                                                    {String(item.duration_ms / 60000)[0]}:
-                                                    {Math.floor((item.duration_ms / 60000 - Math.floor(item.duration_ms / 60000)) * 60) >= 10
-                                                        ? Math.floor((item.duration_ms / 60000 - Math.floor(item.duration_ms / 60000)) * 60)
-                                                        : `0${Math.floor((item.duration_ms / 60000 - Math.floor(item.duration_ms / 60000)) * 60)}`}
-                                                </p>
-                                            </div>
-                                        </Col>
-                                        <Col xs={{ offset: 1, span: 5 }} xl={{ offset: 0, span: 6 }}>
-                                            <div className="song-card-name">{item.name}</div>
-                                            <div className="song-card-artist">{item.artists.map((artist) => artist.name).join(", ")}</div>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                                <Col>
-                                    <button
-                                        onClick={() => {
-                                            client.socket.emit("addToQueue", {
-                                                track: item,
-                                                user: JSON.parse(localStorage.getItem("client-data")).body.display_name,
-                                                newQueueItem: [item, JSON.parse(localStorage.getItem("client-data")).body.display_name],
-                                            });
-                                        }}
-                                    >
-                                        Add
-                                    </button>
-                                </Col>
-                            </Row>
-                        );
-                    })
-                )}
+                {searchResults.map((item, key) => {
+                    return (
+                        <Row key={key} className="song-card">
+                            <Col xs={12} xl={9}>
+                                <Row style={{ width: "100%" }}>
+                                    <Col xs={6}>
+                                        <div style={{ margin: "0.5em", textAlign: "center" }}>
+                                            <img className="unselectable" src={item.album.images[1].url} />
+                                            {String(item.duration_ms / 60000)[0]}:
+                                            {Math.floor((item.duration_ms / 60000 - Math.floor(item.duration_ms / 60000)) * 60) >= 10
+                                                ? Math.floor((item.duration_ms / 60000 - Math.floor(item.duration_ms / 60000)) * 60)
+                                                : `0${Math.floor((item.duration_ms / 60000 - Math.floor(item.duration_ms / 60000)) * 60)}`}
+                                        </div>
+                                    </Col>
+                                    <Col xs={{ offset: 1, span: 5 }} xl={{ offset: 0, span: 6 }}>
+                                        <div className="song-card-name">{item.name}</div>
+                                        <div className="song-card-artist">{item.artists.map((artist) => artist.name).join(", ")}</div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col>
+                                <button
+                                    onClick={() => {
+                                        client.socket.emit("addToQueue", {
+                                            track: item,
+                                            user: JSON.parse(localStorage.getItem("client-data")).body.display_name,
+                                            newQueueItem: [item, JSON.parse(localStorage.getItem("client-data")).body.display_name],
+                                        });
+                                    }}
+                                >
+                                    Add
+                                </button>
+                            </Col>
+                        </Row>
+                    );
+                })}
             </div>
         </div>
     );
